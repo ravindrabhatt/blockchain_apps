58458ac41887fbe59f2d377a0291e853b5e915a1
Ravindra Bhatt <ravindra.bhatt29@gmail.com>
2018-08-05 16:54:17 +0530

    Create react app for lottery contract

---
+++ lottery-react/src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import './App.css';
+import lottery from './lottery';
+
+class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = { manager : ''};
+  }
+
+  async componentDidMount() {
+    const manager = await lottery.methods.manager().call();
+    this.setState({ manager });
+
+  }
+  render() {
+    return (
+      <div>
+        <h2>Lottery contract</h2>
+        <p>This contract is managed by {this.state.manager}</p>
+      </div>  
+    );
+  }
+}
+
+export default App;
