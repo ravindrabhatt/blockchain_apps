ac957ab8b460a9e969fa80b65e34b42ba1d73508
Ravindra Bhatt <ravindra.bhatt29@gmail.com>
2018-08-05 17:33:27 +0530

    Add ability to pick a winner via react app.

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -47,6 +47,11 @@ class App extends Component {
 
         <hr/>
 
+        <h4>Ready to pick a winner</h4>
+        <button onClick={this.onClick}>Pick a winner</button>
+
+        <hr/>
+
         <h1>{this.state.message}</h1>
       </div>  
     );
@@ -65,7 +70,19 @@ class App extends Component {
     });
 
     this.setState({message: 'You have been entered!'});
-  }
+  };
+
+  onClick = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    this.setState({message: 'Waiting on transaction success...'});
+
+    await lottery.methods.pickWinner().send({
+      from: accounts[0]
+    });
+
+    this.setState({message: 'A winner has been picked!'});
+  };
 }
 
 export default App;
