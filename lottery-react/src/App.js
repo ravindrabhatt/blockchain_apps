afa74fbc64d6fce5ef6c911a2b187c57af9e002d
Ravindra Bhatt <ravindra.bhatt29@gmail.com>
2018-08-05 17:07:52 +0530

    Show number of players participating in lottery and amount to be won

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
 import lottery from './lottery';
+import web3 from './web3';
 
 class App extends Component {
 
-  constructor(props){
-    super(props);
-
-    this.state = { manager : ''};
-  }
+  state = { 
+    manager: '',
+    players: [],
+    balance: ''
+  };
 
   async componentDidMount() {
     const manager = await lottery.methods.manager().call();
-    this.setState({ manager });
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
 
+    this.setState({ manager, players, balance });
   }
   render() {
     return (
       <div>
         <h2>Lottery contract</h2>
-        <p>This contract is managed by {this.state.manager}</p>
+        <p>
+          This contract is managed by {this.state.manager}.
+          There are currently {this.state.players.length} people entered,
+          completing to win {web3.utils.fromWei(this.state.balance)} ether!
+        </p>
       </div>  
     );
   }
