cfdb0673e6734bacb2516c20da7008b708f7a538
Ravindra Bhatt <ravindra.bhatt29@gmail.com>
2018-08-18 09:57:10 +0530

    Add routing to pages

--- kickstarter/components/Header.js
+++ kickstarter/components/Header.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import { Menu } from 'semantic-ui-react';
+import { Link } from '../routes';
 
 export default () => {
   return (
   	<Menu style={{ marginTop: '10px' }}>
-  	  <Menu.Item>
-        CrowdCoin
-  	  </Menu.Item>
-      <Menu.Menu position="right">
+      <Link route="/">
+        <a className="item">
+          CrowdCoin
+        </a>
+      </Link>
 
-	      <Menu.Item>
-	        Campaigns
-	      </Menu.Item>
-	      <Menu.Item>
-	        +
-	      </Menu.Item>
+      <Menu.Menu position="right">
+        <Link route="/">
+          <a className="item">
+            Campaigns
+          </a>
+        </Link>
+
+       <Link route="/campaigns/new">
+        <a className="item">
+          +
+        </a>
+       </Link>
+
       </Menu.Menu>
   	</Menu>
   );
